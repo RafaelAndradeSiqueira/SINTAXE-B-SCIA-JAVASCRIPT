c60be8c401ae26dd4d627fd2e75865ae51055b9e
Rafael_Andrade <r-andrade-siqueira@hotmail.com>
2022-05-18 19:11:11 -0300

    first commit

---
+++ aula02/script.js
@@ -0,0 +1,32 @@
+// bolean
+
+var VouF = false;
+console.log(VouF);
+
+//number
+
+var numero = 1;
+console.log(numero);
+
+//nome
+var nome ="Rafa";
+console.log(nome);
+console.log(typeof(nome));
+
+//tipos de declaracao
+
+//var
+//let
+//const
+
+
+var escopoglobal = "oi";
+console.log(escopoglobal);
+
+function escopolocal() {
+    let escopolocalinterno = "local";
+    console.log(escopolocalinterno);
+}
+
+escopolocal();
+
---
+++ aula03/script.js
@@ -0,0 +1,22 @@
+// vetores e arrays
+
+let array = ["oi",true,05];
+console.log(array);
+
+let array2 = ["oi",true,01,[02,03]];
+console.log(array2);
+
+//forEach() - itera um item
+//push()- add item no final do array    
+//pop() - remove item do final do array
+//shift() - remove item do começo do array
+//unshift() - add item no começo do arrya
+//indexOf() - retorna o indíce de um valor
+//splice() - remove ou substitui um item pelo indíce
+//slice() - retorna uma parte de um array existente
+
+let objeto = {string: 'string',number:1, Boolean: true};
+console.log(objeto);
+
+var strings = objeto.string;
+console.log(strings);
